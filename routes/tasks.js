94393677e55bc2497ca212242f817de96321b0af
Batool Zahra <90136703+zhra96@users.noreply.github.com>
2021-11-02 21:15:26 +0500

    Update tasks.js

--- routes/tasks.js
+++ routes/tasks.js
@@ -2,11 +2,10 @@ const express = require('express');
 const router = express.Router();
 const {getAllTasks, createTask,getTask,updateTask,deleteTask} = require('../controllers/tasks')
 
-router.get('/',(req,res)=>{
-    getAllTasks()
-})
-// router.route('/').get(getAllTasks).post(createTask)
-// router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask)
+
+
+router.route('/').get(getAllTasks).post(createTask)
+router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask)
 
 
 module.exports = router ;
