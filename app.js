6254b8dda50125f8eef5a9bfd3b930a9d9221819
Batool Zahra <90136703+zhra96@users.noreply.github.com>
2021-11-02 21:18:12 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
+const connectDB = require('./db/connect');
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
+require('dotenv').config()
+
 
 //middleware
 app.use(express.json())
-
+//app.use(express.urlencoded())
 
 // routes
 app.get('/hello',(req,res)=>{
@@ -16,5 +19,17 @@ app.use('/api/v1/tasks',tasks)
 
 const port = 4000
 
+const start = async ()=>{
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port,console.log(`Server is listening on port ${port}...`))
 
-app.listen(port,console.log(`Server is listening on port ${port}`))
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
+
+start()
+
+
