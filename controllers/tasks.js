9d59daa7a0a40515ecd9ed65f39ec5971a666b5e
Batool Zahra <90136703+zhra96@users.noreply.github.com>
2021-11-02 21:16:31 +0500

    Update tasks.js

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,11 +1,14 @@
+const Task = require('../models/Task')
+
 const getAllTasks = (req,res)=>{
-    res.send('all items')
+    res.send('get all tasks')
 }
-const createTask = (req,res)=>{
-    res.send('create task')
+const createTask = async(req,res)=>{
+    const task = await Task.create(req.body)
+    res.status(201).json({task})
 }
 const getTask = (req,res)=>{
-    res.send('get single task')
+    res.json({id : req.params.id})
 }
 const updateTask = (req,res)=>{
     res.send('update task')
